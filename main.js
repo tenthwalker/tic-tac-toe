16965014782f6e8cb50732a3c5d0f25327de01bf
Arden Ranta <arranta20@gmail.com>
2023-09-20 15:13:32 -0700

    feat/add html bones, preliminary css, and js notes

---
+++ main.js
@@ -0,0 +1,13 @@
+// Query Selectors
+
+// Event Listeners
+
+// Event Handlers
+
+// function to create objects to store player info (id, token, wins)
+// function increaseWins to increase the count of either player's wins
+// function to keep track of the data for the game board
+// function to keep track of player's turns (whose turn is it)
+// function to check game board for win conditions
+// function to detect that the game is a draw (no win conditions met)
+// function to reset the game board and data to begin a new game
