ec38a050f99b2011973b3f035f49a719db0d0778
Arden Ranta <arranta20@gmail.com>
2023-09-25 17:44:52 -0700

    feat/add gamePlay function

--- main.js
+++ main.js
@@ -3,17 +3,31 @@ var display = document.querySelector("#messageBoard");
 var buttons = document.querySelectorAll("button");
 var player2Win = document.querySelector("#p2");
 var player1Win = document.querySelector("#p1");
+var buttonsArray = Array.from(buttons);
+var newPlayer = {};
+var players = [];
+var player1;
+var player2;
+
 
 // Event Listeners
-window.addEventListener("load", createPlayer(1, "&#129384", 0), createPlayer(2, "&#128031", 0));
+window.addEventListener("load", function() {
+  createPlayer(1, "&#129384", 0);
+  createPlayer(2, "&#128031", 0);
+})
+for (var i = 0; i < buttonsArray.length; i++) {
+  buttonsArray[i].addEventListener("click", function(event) {
+    gamePlay(event);
+  });
+};
 
 // Event Handlers
 var playerStart = 1;
-var buttonsArray = Array.from(buttons);
 var currentTurn = 1;
 var buttonsClicked1 = [];
 var buttonsClicked2 = [];
-var players = [];
+var remainingChoices = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
 var winCombos = [
   [0, 1, 2],
   [3, 4, 5],
@@ -24,69 +38,104 @@ var winCombos = [
   [0, 4, 8],
   [2, 4, 6]
 ];
+var currentPlayer;
 
 // function to create objects to store player info (id, token, wins)
-function createPlayer(id, token, wins) {
-  var player = {
-    id: id,
-    token: token,
-    wins: wins
+function createPlayer(number, icon, win) {
+  newPlayer = {
+    id: number,
+    token: icon,
+    wins: win
   }
-  players.push(player);
-}
-var player1 = players[0];
-var player2 = players[1];
-// function increaseWins to increase the count of either player's wins
-function increaseWins(player) {
-  player.wins += 1;
-  if (player.id == 1) {
-    player1Win.innerHTML = player.wins;
-  } else if (player.id == 2) {
-    player2Win.innerHTML = player.wins;
-  }
+  players.push(newPlayer);
+  player1 = players[0];
+  player2 = players[1];
+  currentPlayer = player1;
+  return players;
 }
 
 // function to keep track of the data for the game board
-// if conditional to check if button has been clicked (event.target?)
-// if already clicked, do nothing, if not, update innerHTML of button to players[i].token depending on player's turn and call turnTracker()
-// push clicked buttons to either buttonsClicked1 or 2 depending on players[i] turn
+function gamePlay(event) {
+console.log(event.target, "event.target")
+  if (event.target.classList.contains("clicked") === false) {
+    event.target.classList.add("clicked");
+    event.target.innerHTML = `${currentPlayer.token}`;
+    for (var i = 0; i < remainingChoices.length; i++) {
+      if (i == event.target.id) {
+        remainingChoices.splice(i, 1);
+      }
+     }  
+    if (currentPlayer === player1) {
+      buttonsClicked1.push(event.target.id);
+    } else if (currentPlayer === player2) {
+      buttonsClicked2.push(event.target.id);
+    }
+    currentTurn += 1;
+    win();
+    draw();
+    turnTracker();
+  }
+}
 
 // function to keep track of player's turns (whose turn is it)
 function turnTracker() {
-// if playerStart is odd, player 1 starts, if even, player 2 
-// check for playerStart remainder to determine even or odd
-// update currentTurn variable
-display.innerHTML = ``;
+  if (currentTurn % 2 === 0) {
+    currentPlayer = player2;
+  } else {
+    currentPlayer = player1;
+  }
+display.innerHTML = `It's ${currentPlayer.token}'s turn!`;
+return currentPlayer;
+}
+
+// function increaseWins to increase the count of either player's wins
+function increaseWins() {
+  currentPlayer.wins += 1;
+  if (currentPlayer.id == 1) {
+    player1Win.innerHTML = `wins: ${currentPlayer.wins}`;
+  } else if (currentPlayer.id == 2) {
+    player2Win.innerHTML = `wins: ${currentPlayer.wins}`;
+  }
 }
 
 // function to check game board for win conditions
 function win() {
-// loop through winCombos, check for match to buttonsClicked1 or 2
-for (var i = 0; i < winCombos.length; i++) {
-  if (buttonsClicked1.contains(winCombos[i])) {
-    display.innerHTML = `${player1.token} won!`;
-    increaseWins(player1);
+    console.log(buttonsClicked1, "buttonsclicked1")
+    console.log(buttonsClicked2, "buttonsclicked2")
+  for (var i = 0; i < winCombos.length; i++) {
+    console.log(winCombos[i], "wincombos[i]")
+    if (buttonsClicked1.includes((winCombos[i][0]) && (winCombos[i][2]) && (winCombos[i][3]))) {
+      display.innerHTML = `${player1.token} won!`;
+      increaseWins(player1);
+      reset();
+    }
+    if (buttonsClicked2.includes((winCombos[i][0]) && (winCombos[i][2]) && (winCombos[i][3]))) {
+      display.innerHTML = `${player2.token} won!`;
+      increaseWins(player2);
+      reset();
+    }
   }
-  if (buttonsClicked2.contains(winCombos[i])) {
-    display.innerHTML = `${player2.token} won!`;
-    increaseWins(player2);
-  }
-}
-reset();
 }
 
 // function to detect that the game is a draw (no win conditions met)
 function draw() {
-display.innerHTML = "It's a draw!"
-reset()
+  if (remainingChoices.length < 2) {
+    display.innerHTML = "It's a draw!";
+    reset();
+  }
 }
 
 // function to reset the game board and data to begin a new game
-
 function reset() {
 setTimeout(reset, 3 * 1000);
 display.innerHTML = "";
 playerStart += 1;
+currentTurn = playerStart;
+for (var i = 0; i < buttonsArray.length; i++) {
+  buttonsArray[i].innerHTML = "";
+  buttonsArray[i].classList.remove("clicked");
+}
+remainingChoices = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 buttonsClicked1 = [];
 buttonsClicked2 = [];
 }
